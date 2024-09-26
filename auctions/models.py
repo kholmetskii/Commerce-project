ccbc1fd875aef8657908d1a7c049292e7d52d0bb
kholmetskii <150633190+kholmetskii@users.noreply.github.com>
2024-09-26 18:29:31 +0100

    Added main models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=512)
+    imageUrl = models.CharField(max_length=1024)
+    price = models.FloatField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+
+
+
