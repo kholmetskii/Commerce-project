9a808b534c5423af219a7969116c49380e668503
kholmetskii <150633190+kholmetskii@users.noreply.github.com>
2024-10-01 15:00:01 +0100

    Some models migrations

--- auctions/models.py
+++ auctions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Listing', blank=True, related_name="watchlist")
+    watchlist = models.ManyToManyField('Listing', blank=True, related_name="watchlisted_by")
 
 
 class Category(models.Model):
@@ -18,15 +18,18 @@ class Listing(models.Model):
     description = models.CharField(max_length=512)
     imageUrl = models.CharField(max_length=1024)
     start_price = models.FloatField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="user")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="listings_owned")
     is_active = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True,
+                                 related_name="listings_in_category")
     last_bid = models.ForeignKey('Bid', on_delete=models.SET_NULL, blank=True, null=True,
                                  related_name="last_bid_listing")
 
 
 class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     amount = models.FloatField()
     time_of_posting = models.DateTimeField(auto_now_add=True)
+
+
